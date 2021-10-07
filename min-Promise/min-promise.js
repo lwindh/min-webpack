1089eca74f759b3bbc3c006b7adefec3193783f3
lvh666 <lvhao4748@163.com>
2021-10-07 22:03:12 +0800

    feat(min-promise): add promise
    Signed-off-by: lvh666 <lvhao4748@163.com>

---
+++ min-Promise/min-promise.js
@@ -0,0 +1,222 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+class MyPromise {
+  FULFILLED_CALLBACK_LIST = [];
+  REJECTED_CALLBACK_LIST = [];
+  _status = PENDING;
+
+  constructor(fn) {
+    this.status = PENDING;
+    this.value = null;
+    this.reason = null;
+
+    try {
+      fn(this.resolve.bind(this), this.reject.bind(this));
+    } catch (e) {
+      this.reject(e);
+    }
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  set status(newStatus) {
+    this._status = newStatus;
+    switch (newStatus) {
+      case FULFILLED: {
+        this.FULFILLED_CALLBACK_LIST.forEach((callback) => {
+          callback(this.value);
+        });
+        break;
+      }
+      case REJECTED: {
+        this.REJECTED_CALLBACK_LIST.forEach((callback) => {
+          callback(this.reason);
+        });
+        break;
+      }
+    }
+  }
+
+  resolve(value) {
+    if (this.status === PENDING) {
+      this.value = value;
+      this.status = FULFILLED;
+    }
+  }
+
+  reject(reason) {
+    if (this.status === PENDING) {
+      this.reason = reason;
+      this.status = REJECTED;
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    const fulFilledFn = this.isFunction(onFulfilled)
+      ? onFulfilled
+      : (value) => {
+          return value;
+        };
+    const rejectedFn = this.isFunction(onRejected)
+      ? onRejected
+      : (reason) => {
+          throw reason;
+        };
+    const fulFilledFnWithCatch = (resolve, reject, newPromise) => {
+      queueMicrotask(() => {
+        try {
+          if (!this.isFunction(onFulfilled)) {
+            resolve(this.value);
+          } else {
+            const x = fulFilledFn(this.value);
+            this.resolvePromise(newPromise, x, resolve, reject);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      });
+    };
+
+    const rejectedFnWithCatch = (resolve, reject, newPromise) => {
+      queueMicrotask(() => {
+        try {
+          if (!this.isFunction(onRejected)) {
+            reject(this.reason);
+          } else {
+            const x = rejectedFn(this.reason);
+            this.resolvePromise(newPromise, x, resolve, reject);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      });
+    };
+
+    switch (this.status) {
+      case FULFILLED: {
+        const newPromise = new MyPromise((resolve, reject) =>
+          fulFilledFnWithCatch(resolve, reject, newPromise)
+        );
+        return newPromise;
+      }
+      case REJECTED: {
+        const newPromise = new MyPromise((resolve, reject) =>
+          rejectedFnWithCatch(resolve, reject, newPromise)
+        );
+        return newPromise;
+      }
+      case PENDING: {
+        const newPromise = new MyPromise((resolve, reject) => {
+          this.FULFILLED_CALLBACK_LIST.push(() =>
+            fulFilledFnWithCatch(resolve, reject, newPromise)
+          );
+          this.REJECTED_CALLBACK_LIST.push(() =>
+            rejectedFnWithCatch(resolve, reject, newPromise)
+          );
+        });
+        return newPromise;
+      }
+    }
+  }
+
+  resolvePromise(newPromise, x, resolve, reject) {
+    if (newPromise === x) {
+      return reject(
+        new TypeError("The promise and the return value are the same")
+      );
+    }
+
+    if (x instanceof MyPromise) {
+      if (x.status === PENDING) {
+        x.then(
+          (y) => {
+            this.resolvePromise(newPromise, y, resolve, reject);
+          },
+          (reason) => {
+            reject(reason);
+          }
+        );
+      } else {
+        x.then(resolve, reject);
+      }
+    } else if (typeof x === "object" || this.isFunction(x)) {
+      if (x === null) {
+        return resolve(x);
+      }
+
+      let then = null;
+
+      try {
+        then = x.then;
+      } catch (error) {
+        return reject(error);
+      }
+
+      if (this.isFunction(then)) {
+        let called = false;
+        try {
+          then.call(
+            x,
+            (y) => {
+              if (called) return;
+              called = true;
+              this.resolvePromise(newPromise, y, resolve, reject);
+            },
+            (r) => {
+              if (called) return;
+              called = true;
+              reject(r);
+            }
+          );
+        } catch (error) {
+          if (called) return;
+          reject(error);
+        }
+      } else {
+        resolve(x);
+      }
+    } else {
+      resolve(x);
+    }
+  }
+
+  isFunction(param) {
+    return typeof param === "function";
+  }
+
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+
+  static resolve(value) {
+    if (value instanceof MyPromise) {
+      return value;
+    }
+
+    return new MyPromise((resolve) => {
+      resolve(value);
+    });
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason);
+    });
+  }
+}
+
+const test = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    reject(1);
+  }, 1000);
+}).catch((reason) => console.log("报错" + reason));
+
+console.log(test);
+
+setTimeout(() => {
+  console.log(test);
+}, 2000);
