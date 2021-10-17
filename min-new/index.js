ad1738fc8a7ad54017d63bffda573ce83dc36b99
lvh666 <lvhao4748@163.com>
2021-10-17 20:33:04 +0800

    refactor(min-new): change code form
    Signed-off-by: lvh666 <lvhao4748@163.com>

--- min-new/index.js
+++ min-new/index.js
@@ -1,5 +1,6 @@
 const Mynew = (obj, ...rest) => {
-    let o = new Object();
+    // let o = new Object();
+    let o = Object.create(null);
     o.__proto__ = obj.prototype;
 
     let res = obj.apply(o, rest);
