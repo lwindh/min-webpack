5cea40ef0732fb97aa3ff6b7fb8cfa563c3fa780
lwindh <lvhao4748@163.com>
2021-10-22 09:57:33 +0800

    refactor(min-bind): add description

--- min-bind/index.js
+++ min-bind/index.js
@@ -1,11 +1,14 @@
+// for split arguments
 const ArrayPrototypeSlice = Array.prototype.slice;
+// realize bind function
 Function.prototype.bind = function (otherThis) {
+	// determine whether the object used is a function
   if (typeof this !== "function") {
     throw new TypeError(
       "Function.prototype.bind - what is trying to be bound is not callable"
     );
   }
-
+	// split arguments as a variable
   let baseArgs = ArrayPrototypeSlice.call(arguments, 1),
     baseArgsLength = baseArgs.length,
     fToBind = this,
@@ -18,7 +21,7 @@ Function.prototype.bind = function (otherThis) {
         baseArgs
       );
     };
-
+  // extends function
   if (this.prototype) {
     fNOP.prototype = this.prototype;
   }
