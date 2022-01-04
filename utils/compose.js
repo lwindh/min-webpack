408a20d57ae0741209d5570be4f7b754e410942a
lwindh <lvhao4748@163.com>
2022-01-04 09:52:39 +0800

    feat(compose): add compose function

---
+++ utils/compose.js
@@ -0,0 +1,24 @@
+const compose = (...fn) => {
+  if (!fn.length) return (value) => value
+  if (fn.length === 1) return fn[0]
+  return fn.reduce(
+    (pre, cur) =>
+      (...args) =>
+        pre(cur(...args))
+  )
+}
+
+function fn1(x) {
+  return x + 1
+}
+function fn2(x) {
+  return x + 2
+}
+function fn3(x) {
+  return x + 3
+}
+function fn4(x) {
+  return x + 4
+}
+const a = compose(fn1, fn2, fn3, fn4)
+console.log(a(1))
