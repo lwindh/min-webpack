7e7ff63ef8c3b2c315befae55807be4030e370c1
lwindh <lvhao4748@163.com>
2022-01-13 11:01:02 +0800

    refactor(extends): add extends history development

--- utils/extends.js
+++ utils/extends.js
@@ -1,3 +1,48 @@
+// 原型链继承
+// 问题1：原型中包含的引用类型属性将被所有实例共享
+// 问题2：子类在实例化的时候不能给父类构造函数传参
+function Animal() {
+  this.colors = ['black', 'white']
+}
+Animal.prototype.getType = function () {
+  return this.colors
+}
+
+function Dog() {}
+Dog.prototype = new Animal()
+
+// 借用构造函数实现继承
+// 问题1：导致每次创建子类实例都会创建一遍方法
+function Animal(name) {
+  this.name = name
+  this.getName = function () {
+    return this.name
+  }
+}
+
+function Dog(name) {
+  Animal.call(this, name)
+}
+Dog.prototype = new Animal()
+
+// 组合继承
+// 问题1：调用了 2 次父类构造函数
+function Animal(name) {
+  this.name = name
+  this.colors = ['black', 'white']
+}
+Animal.prototype.getName = function() {
+  return this.name
+}
+
+function Dog(name, age) {
+  Animal.call(this, name)
+  this.age = age
+}
+Dog.prototype =  new Animal()
+Dog.prototype.constructor = Dog
+
+// 寄生式组合继承
 function Parent(name) {
   this.name = name
   this.say = () => {
