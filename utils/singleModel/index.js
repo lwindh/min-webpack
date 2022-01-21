2b9627c28a4c0df6a413ab1a3835d931b715fd86
lwindh <lvhao4748@163.com>
2022-01-21 09:06:55 +0800

    refactor(singleModel): add singleModel complete way

--- utils/singleModel/index.js
+++ utils/singleModel/index.js
@@ -15,7 +15,39 @@ const FooServiceSingleton = (function () {
     },
   };
 })();
-const fooService1 = FooServiceSingleton.getInstance();
-const fooService2 = FooServiceSingleton.getInstance();
+
+const FooServiceSingleton1 = (function () {
+  // 未初始化的单例对象
+  let fooService;
+  // 创建/获取单例对象的函数
+  const CreateInstance = function (name) {
+    if (fooService) {
+      return fooService;
+    }
+    this.name = name
+    return fooService = this;
+  }
+
+  return CreateInstance;
+})();
+
+const CreateInstance = function (name) {
+  this.name = name
+}
+
+const FooServiceSingleton2 = (function () {
+  // 未初始化的单例对象
+  let fooService;
+
+  return function (name) {
+    if (!fooService) {
+      fooService = new CreateInstance(name);
+    }
+    return fooService;
+  };
+})();
+
+const fooService1 = new FooServiceSingleton2('1');
+const fooService2 = new FooServiceSingleton2('2');
 
 console.log(fooService1 === fooService2);
