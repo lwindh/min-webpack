14d9a39f5e8de3871c92411e4cd2af7137074f1e
lwindh <lvhao4748@163.com>
2022-02-16 09:08:59 +0800

    feat(JSON.parse): add JSON.parse

---
+++ utils/JSON_parse.js
@@ -0,0 +1,20 @@
+//1
+const json = '{"a":"1", "b":2}'
+const obj = eval('(' + json + ')')
+
+const rx_one = /^[\],:{}\s]*$/
+const rx_two = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g
+const rx_three =
+  /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g
+const rx_four = /(?:^|:|,)(?:\s*\[)+/g
+
+if (
+  rx_one.test(
+    json.replace(rx_two, '@').replace(rx_three, ']').replace(rx_four, '')
+  )
+) {
+  const obj = eval('(' + json + ')')
+}
+
+//2
+const obj2 = new Function('return ' + json)()
