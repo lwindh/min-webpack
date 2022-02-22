a377306ad0a049c9fc9bef20423bec453f9534e4
lwindh <lvhao4748@163.com>
2022-02-22 08:55:17 +0800

    feat(lru): add lru

---
+++ utils/LRU.js
@@ -0,0 +1,28 @@
+class LRUCache {
+  constructor(size) {
+    this.size = size
+    this.cache = new Map()
+  }
+
+  get(key) {
+    const hasKey = this.cache.has(key)
+    if (hasKey) {
+      const val = this.cache.get(key)
+      this.cache.delete(key)
+      this.cache.set(key, val)
+      return val
+    }
+    return -1
+  }
+
+  put(key, val) {
+    const hasKey = this.cache.has(key)
+    if (hasKey) {
+      this.cache.delete(key)
+    }
+    this.cache.set(key, val)
+    if (this.cache.size > this.size) {
+      this.cache.delete(this.cache.keys().next().value)
+    }
+  }
+}
