d5cabdc46e09efbd72394f4850a0c7badcdbd056
lwindh <lvhao4748@163.com>
2022-02-28 09:27:28 +0800

    refactor(utils): 新增去重实现方法

--- utils/unique.js
+++ utils/unique.js
@@ -6,4 +6,16 @@ const unique = (arr) => {
   return res
 }
 
+const unique2 = (arr) => {
+  const newArr = []
+  arr.reduce((pre, next) => {
+    if (!pre.has(next)) {
+      pre.set(next, 1)
+      newArr.push(next)
+    }
+    return pre
+  }, new Map())
+  return newArr
+}
+
 const uniqueArr = (arr) => [...new Set(arr)]
